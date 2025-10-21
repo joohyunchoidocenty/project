0f3d86cce1543f41b7d24f18bd7f4f5d658c0698
Joohyun choi <joohyun.choi@docenty.ai>
2025-10-21 12:02:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI 
 
 app = FastAPI() 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 개발 중엔 * 허용 (모든 도메인 접근 가능)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/") 
 
